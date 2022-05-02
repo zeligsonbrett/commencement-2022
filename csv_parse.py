33372799f9fffb740534e21f55b0a0a3f478b361
zeligsongit <zeligson@princeton.edu>
2022-05-01 21:13:19 -0400

    python

--- csv_parse.py
+++ csv_parse.py
@@ -4,5 +4,5 @@ with open('2022 names - Sheet1.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     
     for row in csv_reader:
-        current = '<div class = "name">' + row[0] + '</div>\n'
+        current = '<div class = "name">' + row[0] + '</div>'
         print(current)
