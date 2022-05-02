cd04ecaf145f9a7a7665f05613bd8f32b6dc8a66
zeligsongit <zeligson@princeton.edu>
2022-05-01 21:11:30 -0400

    python

--- csv_parse.py
+++ csv_parse.py
@@ -1,6 +1,6 @@
 import csv
 
-with open('2022 names - Sheet1') as csv_file:
+with open('2022 names - Sheet1.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     
     for row in csv_reader:
